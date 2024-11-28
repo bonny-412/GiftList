b1f8e1d1f5e04668b2a644d84115ed24fc1689e9
Andrea <a.bonaiuti@datapos.it>
2024-11-28 12:02:18 +0100

    aggiunta marsupi

--- src/app/components/gifts/gifts.component.ts
+++ src/app/components/gifts/gifts.component.ts
@@ -48,6 +48,34 @@ export class GiftsComponent {
       color: '-',
       size: 'L/XL'
     },
+    {
+      title: 'Zaino Pieghevole Macna Hipbag',
+      url: 'https://www.xlmoto.it/product/zaino-pieghevole-macna-hipbag_pid-PIA-349513',
+      img: 'macna-zaino.png',
+      color: '-',
+      size: '-'
+    },
+    {
+      title: 'Marsupio Macna',
+      url: 'https://www.xlmoto.it/product/marsupio-macna-25x8x13cm-nero_pid-PIA-163747',
+      img: 'macna-marsupio.png',
+      color: '-',
+      size: '-'
+    },
+    {
+      title: 'Marsupio Race Now',
+      url: 'https://www.wheelup.it/marsupio-race-now-1-5-litri-nero.html?utm_source=google&utm_medium=cpc&utm_campaign=PMax%3A%202021_IT_Shopping_Smart&utm_id=17820149233&gad_source=1&gbraid=0AAAAAC4p4IfsKY52TRi8vIanp7qiKrW_4&gclid=CjwKCAiAxqC6BhBcEiwAlXp459h_fuIS0BI8rx4rr8NdeypboM_pE62WtOnkqNOMHlHrbQE3me96ZhoCZ14QAvD_BwE',
+      img: 'marsupio-race-now.png',
+      color: '-',
+      size: '-'
+    },
+    {
+      title: 'Marsupio B9',
+      url: 'https://www.wheelup.it/marsupio-b9-3-5-litri-nero.html',
+      img: 'marsupio-b9.png',
+      color: '-',
+      size: '-'
+    },
   ]
 
 }
