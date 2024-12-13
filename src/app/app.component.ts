eac88b06e00f0614f5fc3ab4be5eb70f919a60d6
Andrea <a.bonaiuti@datapos.it>
2024-12-13 10:13:49 +0100

    Aggiunta effetto di natale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CountdownComponent } from './components/countdown/countdown.component';
 import { GiftsComponent } from './components/gifts/gifts.component';
@@ -12,6 +12,54 @@ import { ButtonSoundComponent } from './components/button-sound/button-sound.com
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  santaElement!: HTMLElement;
+  isChristmas:boolean = false;
+  isBirthday:boolean = false;
+
+  ngOnInit(): void {
+    this.checkIfChristmas();
+    this.checkIfBirthday();
+
+    if(this.isChristmas) {
+      this.santaElement = document.getElementById('santa')!;
+      this.moveSanta(); // Prima posizione iniziale
+      setInterval(() => this.moveSanta(), 3000); // Cambia posizione ogni 3 secondi
+    }
+
+    if(this.isBirthday) {
+      // this.santaElement = document.getElementById('santa')!;
+      // this.moveSanta(); // Prima posizione iniziale
+      // setInterval(() => this.moveSanta(), 3000); // Cambia posizione ogni 3 secondi
+    }
+  }
+
+  moveSanta(): void {
+    const { x, y } = this.getRandomPosition();
+    this.santaElement.style.transform = `translate(${x}px, ${y}px)`;
+  }
+
+  getRandomPosition(): { x: number; y: number } {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const santaWidth = this.santaElement.offsetWidth;
+    const santaHeight = this.santaElement.offsetHeight;
+
+    const x = Math.random() * (windowWidth - santaWidth);
+    const y = Math.random() * (windowHeight - santaHeight);
+
+    return { x, y };
+  }
+
+  checkIfChristmas(): void {
+    const today = new Date();
+    this.isChristmas = today.getDate() === 25 && (today.getMonth() + 1) === 12; // Mese 12 = Dicembre
+  }
+
+  checkIfBirthday(): void {
+    const today = new Date();
+    this.isBirthday = today.getDate() === 27 && (today.getMonth() + 1) === 12; // Mese 12 = Dicembre
+  }
+  
 
 }
